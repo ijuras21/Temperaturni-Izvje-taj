dd0e1ffd4a03f8e6d83d932500dc0a74901268c0
Ivan Juras <ijuras21@foi.hr>
2023-10-26 20:01:59 +0200

    Implementirana klasa SensorSystem

---
+++ Temperaturni Izvještaj/SensorSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperaturni_Izvještaj {
+    internal class SensorSystem {
+        private Sensor primary = new Sensor("Sensor-01");
+        private Sensor secondary = new Sensor("Sensor-02");
+
+        public double[] GetHourlyTemperatures() {
+            double[] primaryData = primary.FetchData();
+            double[] secondaryData = secondary.FetchData();
+
+            double[] combinedData = CombineData(primaryData, secondaryData);
+
+            var converter = new Converter(Unit.Fahrenheit, Unit.Celsius);
+            double[] temperatures = converter.Convert(combinedData);
+
+            return temperatures;
+        }
+
+        private double[] CombineData(double[] primaryData, double[] secondaryData) {
+            double[] combinedData = new double[24];
+            for (int hourIndex = 0; hourIndex < 24; hourIndex++)
+            {
+                combinedData[hourIndex] = CalculateTemperature(primaryData, secondaryData, hourIndex);
+            }
+            return combinedData;
+        }
+    }
+}
