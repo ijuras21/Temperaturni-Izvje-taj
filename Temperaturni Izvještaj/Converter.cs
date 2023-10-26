53b48493052aa3356fbbb0142b692e9b7f19b65b
Ivan Juras <ijuras21@foi.hr>
2023-10-26 18:41:58 +0200

    Implementacija klase Converter

---
+++ Temperaturni Izvještaj/Converter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperaturni_Izvještaj {
+
+    enum Unit {
+        Celsius,
+        Fahrenheit
+    }
+
+    internal class Converter {
+        private Unit from;
+        private Unit to;
+
+        public Converter(Unit from, Unit to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        public double[] Convert(double[] temperatures) {
+            double[] convertedData = new double[temperatures.Length];
+            if(from == Unit.Fahrenheit && to == Unit.Celsius) {
+                for(int i=0; i<temperatures.Length; i++) {
+                    convertedData[i] = (temperatures[i] - 32) * 5 / 9;
+                }
+            } else {
+                throw new NotSupportedException();
+            }
+            return convertedData;
+        }
+    }
+}
