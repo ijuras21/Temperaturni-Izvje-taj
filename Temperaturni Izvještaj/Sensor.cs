dc40427939d3af03f1b505a2769a32aa8e080c17
Ivan Juras <ijuras21@foi.hr>
2023-10-26 18:54:18 +0200

    Implementacija klase Sensor

---
+++ Temperaturni Izvještaj/Sensor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperaturni_Izvještaj {
+    internal class Sensor {
+        public string Identifier { get; set; }
+
+        public Sensor (string identifier) {
+            Identifier = identifier;
+        }
+
+        public double[] FetchData() {
+            double[] data = new double[24];
+            var r = new Random();
+            for(int i=0; i<24; i++) {
+                data[i] = r.Next(30, 60) + r.NextDouble();
+            }
+            return data;
+        }
+
+    }
+}
